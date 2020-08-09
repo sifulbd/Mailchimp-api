5943813b57dcddcf15581cc3ae0868a4a6c86eb6
Siful Islam <sifulsmm@gmail.com>
2020-08-09 19:38:56 +0600

    files added

---
+++ app.js
@@ -0,0 +1,67 @@
+//jshint esversion: 6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+const app = express();
+const port = 3001;
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static("public"));
+
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/signup.html");    
+});
+
+app.post("/", function(req, res){    
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const email = req.body.email;
+
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    }
+    const jsonData = JSON.stringify(data);
+    const url = "https://us17.api.mailchimp.com/3.0/lists/98fc5d24dd";
+    const options = {
+        method : "POST",
+        auth: "siful:cb8e5140ed7c2008191ba82249bb7333-us17"
+    }
+    const request = https.request(url, options, function(response) {
+        if(response.statusCode === 200) {
+            res.sendFile(__dirname + "/success.html")
+        }else {
+            res.sendFile(__dirname + "/failure.html")
+        }
+        response.on("data", function(data) {
+            const df = JSON.parse(data);
+            console.log(df);
+        })
+    })
+    request.write(jsonData);
+    request.end();
+})
+
+app.post("/failure", function(req,res){
+    res.redirect("/");
+})
+
+app.listen(port, () => {
+    console.log("app is listening...")
+})
+
+// api key: cb8e5140ed7c2008191ba82249bb7333-us17
+// audience id=: 98fc5d24dd
